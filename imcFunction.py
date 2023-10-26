8cdff4155ca06928780490fce7311677ed4c6ad1
Fernando Marques dos Santos <89764537+TakeruuDEV@users.noreply.github.com>
2023-10-26 13:44:29 -0300

    Update and rename imcFunction.py to analise.py

--- imcFunction.py
+++
@@ -1,9 +0,0 @@
-def calcular_imc(peso, altura):
-    imc = peso / (altura ** 2)
-    return imc
-peso = float(input("Digite o seu peso (kg): "))
-altura = float(input("Digite a sua altura (m): "))
-
-imc = calcular_imc(peso, altura)
-
-print(f"Seu IMC é: {imc:.2f}")
